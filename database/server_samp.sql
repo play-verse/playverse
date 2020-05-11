542d9163d38a74837dfb6fea0e223b50f269f872
Nathan Ramli <hezkyanatanael84@gmail.com>
2020-05-11 23:34:08 +0700

    update db struktur untuk multiple skin

--- database/server_samp.sql
+++ database/server_samp.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100408
  File Encoding         : 65001
 
- Date: 10/05/2020 20:22:30
+ Date: 11/05/2020 23:33:44
 */
 
 SET NAMES utf8mb4;
@@ -111,7 +111,7 @@ CREATE TABLE `user`  (
 -- ----------------------------
 INSERT INTO `user` VALUES (22, 'cosinus', '6E1789AD7F6CFF1BAF1DA2A6B7745F9F6CA6F0F3CCDBA5C97FC40EB22EF7793C', 125, 55, '2020-04-24 21:12:03', 0, 0, 'nathan@gmail.com', 0, '220.573135', '-1890.626953', '3.699802', '31.708591', '0', '0', '621234', 4);
 INSERT INTO `user` VALUES (23, 'Anxitail', '465EBC8A47CC6776C8131DC0EA4EA26B621D72E4B86852B0D51F7A14ACBBA214', 24, 1, '2020-04-25 16:48:59', 100, 0, 'kolak@gmail.com', 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0);
-INSERT INTO `user` VALUES (24, 'cosine', '2308812CE036BE27F4D6818D366094F107A5DB381F4B91973A7A4F6DA4AE1557', 21, 69, '2020-04-30 15:31:48', 1680, 0, 'natan@gmail.com', 0, '154.740036', '-1930.083740', '3.773438', '359.411499', '0', '0', '629876', 1);
+INSERT INTO `user` VALUES (24, 'cosine', '2308812CE036BE27F4D6818D366094F107A5DB381F4B91973A7A4F6DA4AE1557', 19, 76, '2020-04-30 15:31:48', 175040, 0, 'natan@gmail.com', 0, '501.013092', '-1354.233643', '16.132813', '190.002869', '0', '0', '629876', 1);
 INSERT INTO `user` VALUES (25, 'cosines', '9E3645C36D5625B86030BC447A51771E48B0C1D82360E4FCFD15AE896407663B', 76, 4, '2020-05-03 01:51:46', 0, 1, 'nathan@gmail.com', 0, '299.019104', '-2026.331421', '1.413125', '1.111884', '0', '0', NULL, 0);
 INSERT INTO `user` VALUES (26, 'cosinec', '4673452E1D20E8417166B9FF852DC48246F1D1D24FD11076976A3DCB4307675B', 298, 3, '2020-05-03 16:56:12', 0, 1, 'nathan@gmail.com', 0, '188.238831', '-1935.149414', '-0.552782', '273.730988', '0', '0', NULL, 0);
 INSERT INTO `user` VALUES (27, 'cosiozo', 'EEF3ABEA0977171744D9AC2BF8A4761A389F8C55136BDC00B02E9E49524340B1', 9, 1, '2020-05-10 16:59:42', 100, 1, 'asd2@gmail.com', 0, '285.288879', '-1863.428467', '2.890330', '309.904419', '0', '0', NULL, 0);
@@ -153,19 +153,23 @@ CREATE TABLE `user_skin`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `id_user` bigint(20) UNSIGNED NOT NULL,
   `id_skin` int(20) NOT NULL,
+  `jumlah` int(10) NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 38 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 41 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user_skin
 -- ----------------------------
-INSERT INTO `user_skin` VALUES (31, 22, 125);
-INSERT INTO `user_skin` VALUES (32, 22, 28);
-INSERT INTO `user_skin` VALUES (33, 23, 24);
-INSERT INTO `user_skin` VALUES (34, 24, 21);
-INSERT INTO `user_skin` VALUES (35, 25, 76);
-INSERT INTO `user_skin` VALUES (36, 26, 298);
-INSERT INTO `user_skin` VALUES (37, 27, 9);
+INSERT INTO `user_skin` VALUES (31, 22, 125, 1);
+INSERT INTO `user_skin` VALUES (32, 22, 28, 1);
+INSERT INTO `user_skin` VALUES (33, 23, 24, 1);
+INSERT INTO `user_skin` VALUES (34, 24, 21, 1);
+INSERT INTO `user_skin` VALUES (35, 25, 76, 1);
+INSERT INTO `user_skin` VALUES (36, 26, 298, 1);
+INSERT INTO `user_skin` VALUES (37, 27, 9, 1);
+INSERT INTO `user_skin` VALUES (38, 24, 73, 1);
+INSERT INTO `user_skin` VALUES (39, 24, 17, 1);
+INSERT INTO `user_skin` VALUES (40, 24, 19, 6);
 
 -- ----------------------------
 -- Procedure structure for tambah_item
@@ -183,6 +187,27 @@ BEGIN
 		END IF;
 	ELSE
 		INSERT INTO `user_item`(id_item, id_user, jumlah) VALUES(`x_id_item`, `x_id_user`, `x_banyak_item`); 
+	END IF;
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Procedure structure for tambah_skin
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `tambah_skin`;
+delimiter ;;
+CREATE PROCEDURE `tambah_skin`(`x_id_user` int,`x_id_skin` int,`x_banyak_skin` int)
+BEGIN
+	SELECT jumlah, id INTO @jumlah, @id FROM `user_skin` WHERE `id_skin` = `x_id_skin` AND `id_user` = `x_id_user`;
+	IF(ROW_COUNT()) THEN
+		IF(x_banyak_skin < 0 AND (@jumlah - x_banyak_skin) < 1) THEN
+			DELETE FROM `user_skin` WHERE `id` = @id;
+		ELSE
+			UPDATE `user_skin` SET `jumlah` = `jumlah` + `x_banyak_skin` WHERE `id` = @id;
+		END IF;
+	ELSE
+		INSERT INTO `user_skin`(id_skin, id_user, jumlah) VALUES(`x_id_skin`, `x_id_user`, `x_banyak_skin`); 
 	END IF;
 END
 ;;
