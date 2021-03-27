b766eeb0ec18b81f9ef795ecb7530e4521eeaa72
Nathan Ramli <hezkyanatanael84@gmail.com>
2021-03-28 00:53:26 +0700

    update gaji faction + add function loadDynamicRemoveBuilding

---
+++ database/user_count_a_week.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 21, 2021 at 07:55 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `server_samp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_count_a_week`
+--
+
+CREATE TABLE `user_count_a_week` (
+  `id_user` bigint(20) NOT NULL,
+  `last_tanggal` date DEFAULT NULL,
+  `cnt_medic` int(11) DEFAULT 0,
+  `cnt_police` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_count_a_week`
+--
+ALTER TABLE `user_count_a_week`
+  ADD PRIMARY KEY (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
